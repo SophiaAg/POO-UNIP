8cf25bfba4f78d47afa63bc5ce7cd6ea13f32887
Sophia Araújo <116041632+SophiaAg@users.noreply.github.com>
2026-08-28 21:40:35 -0300

    Add files via upload

---
+++ ExerciciosTeams/src/br/unip/sicc/exercicio/view/JanelaBorderLayout.java
@@ -0,0 +1,50 @@
+package br.unip.sicc.exercicio.view;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class JanelaBorderLayout extends JFrame {
+	private JPanel painelCadastro;
+	private JPanel painelBusca;
+	
+	private JanelaBorderLayout() {
+		//Definir o título da janela
+		this.setTitle("Controle de Cartas");
+		//Configurar a dimensão
+		this.setSize(800, 300);
+		// encerramento do processo ao fechar 
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//centralizar a janela 
+		this.setLocationRelativeTo(null);
+		
+		painelCadastro = montaPainelCadastro();
+		painelBusca = montaPainelBusca();
+		
+		this.add(painelCadastro, BorderLayout.WEST);
+		this.add(painelBusca, BorderLayout.CENTER);
+		
+	}
+	
+	private JPanel montaPainelBusca() {
+		JPanel painelBusca = new JPanel();
+		painelBusca.setBackground(Color.WHITE);
+		return painelBusca;
+		
+	}
+	
+	private JPanel montaPainelCadastro() {
+		JPanel painelCadastro = new JPanel();
+		painelCadastro.setBackground(Color.BLACK);
+		return painelCadastro;
+	}
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> {
+			new JanelaBorderLayout().setVisible(true);
+		});
+	}
+}
---
+++ ExerciciosTeams/src/br/unip/sicc/exercicio/view/PainelCadastro.java
@@ -0,0 +1,64 @@
+package br.unip.sicc.exercicio.view;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+//A classe deve herdar de JPanel.
+public class PainelCadastro extends JPanel{
+	
+	private JLabel lblld;
+	private JLabel lblNome;
+	private JTextField textld;
+	private JTextField textNome;
+	private JButton btnSalvar;
+	private JButton btnCancelar;
+	
+	//Construtor PainelCadastro():
+	//instanciar = new
+   private PainelCadastro() {
+	   this.setLayout(new BorderLayout());
+	   JPanel painelCadastro = montaPainelCadastro();
+	   JPanel painelBotoes =  montaPainelBotoes();
+	   
+	   this.add(painelCadastro, BorderLayout.NORTH);
+	   this.add(painelBotoes, BorderLayout.SOUTH);
+	   
+   }
+	
+   private JPanel montaPainelCadastro() {
+	   //nstanciar e retornar um JPanel com layout 
+	   //GridLayout(2, 2) (2 linhas por 2 colunas).
+	      JPanel painelCadastro = new JPanel();
+	      GridLayout layout = new GridLayout(2, 2);
+	      painelCadastro.setLayout(layout);
+	      
+	      lblld = new JLabel("id");
+	      textld = new JTextField("id");
+	      setEnabled(false);
+	      lblNome = new JLabel("Nome");
+	      textNome = new JTextField("Nome");
+	      //Configurar a largura visível do campo de texto txtNome para 10 colunas.
+	      textNome.setColumns(10);
+	      
+	      painelCadastro.add(lblld);
+	      painelCadastro.add(textld);
+	      painelCadastro.add(lblNome);
+	      painelCadastro.add(textNome);
+	      
+		return painelCadastro;
+   }
+
+   private JPanel montaPainelBotoes()  {
+	   JPanel painelBotoes = new JPanel();
+	   FlowLayout layout = new FlowLayout.RIGHT;
+	   return null;
+   }
+   
+}
+
